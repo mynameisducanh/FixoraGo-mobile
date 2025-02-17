195ddaa7af3b7688aa25274633b9feb6d22f7305
DucAnh9981 <anhnguyeduc331@gmail.com>
2025-02-17 10:20:44 +0700

    update base

---
+++ types/user.tsx
@@ -0,0 +1,18 @@
+interface UserInterface {
+  createAt: string;
+  deleteAt: string | null;
+  email: string;
+  emailVerified: number;
+  firstName: string;
+  fullName: string;
+  id: string;
+  lastName: string;
+  roles: string;
+  updateAt: string;
+  userFolderId: string;
+  username: string;
+}
+
+export {
+    UserInterface
+}
