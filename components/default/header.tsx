659902a305cf62c13fc9ab2ffdbf1a55f36cc206
DucAnh9981 <anhnguyeduc331@gmail.com>
2025-02-28 21:34:18 +0700

    update ui welcomescreen

--- components/default/header.tsx
+++ components/default/header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
   const { user, isAuthenticated } = useUserStore();
   return (
     <>
-      <View className="mx-4 flex-row justify-between items-center mb-2">
+      <View className="mx-4 flex-row justify-between items-center mb-6">
         <Image
           source={require("../../assets/images/splash-icon.png")}
           className="rounded-full"
@@ -19,22 +19,22 @@ const Header = () => {
         <FontAwesome name="bell-o" size={24} color="gray" />
       </View>
       <View className="mx-4 space-y-2 mb-2">
-        <Text style={{ fontSize: hp(1.9) }} className="text-neutral-600">
-          Hello , Nguyễn Đức Anh{user?.username}
+        <Text style={{ fontSize: hp(2.0) }} className="text-neutral-600">
+          Xin chào , Nguyễn Đức Anh{user?.username}
         </Text>
         <View>
           <Text
             style={{ fontSize: hp(3.8) }}
             className="font-semibold text-neutral-600"
           >
-            Lorem ipsum dolor sit,
+            Có vấn đề cần sửa chữa ,
           </Text>
         </View>
         <Text
           style={{ fontSize: hp(3.8) }}
           className="font-semibold text-neutral-600"
         >
-          stay at <Text className="text-amber-400">home</Text>
+          đặt ngay <Text className="text-primary">Fixorago</Text>
         </Text>
       </View>
     </>
