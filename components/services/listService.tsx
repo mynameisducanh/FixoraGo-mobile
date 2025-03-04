9f43b31abe0a76698159e22f513e269e13425539
DucAnh9981 <anhnguyeduc331@gmail.com>
2025-03-04 16:27:27 +0700

    update icon service

--- components/services/listService.tsx
+++ components/services/listService.tsx
@@ -6,20 +6,55 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
-import { categoryData } from "@/temp/service";
+import React, { useEffect, useState } from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import Animated, { FadeInDown } from "react-native-reanimated";
 import LottieView from "lottie-react-native";
-import splashIcon from "../../assets/icons/plumbing-icon.json";
-const ListService = ({ activeCategory, setActiveCategory }) => {
+import ApiIconsLottie from "@/api/iconsLottie";
+import { IconLottieInterface } from "@/types";
+
+const ListService = () => {
+  const [animations, setAnimations] = useState({});
+  const [activeCategory, setActiveCategory] = useState<string | null>("Dọn dẹp");
+  const [activeCategory2, setActiveCategory2] = useState<IconLottieInterface[]>([]);
+
+  const api = new ApiIconsLottie();
+  useEffect(() => {
+    const fetchAnimations = async () => {
+      try {
+        const res = await api.getLottie();
+        setActiveCategory2(res);
+
+        const results = await Promise.allSettled(
+          res.map(async (cat: any) => {
+            const response = await fetch(cat.url);
+            if (!response.ok) throw new Error(`Failed to load ${cat.name}`);
+            const json = await response.json();
+            return { [cat.name]: json };
+          })
+        );
+
+        const fulfilledResults = results
+          .filter((r) => r.status === "fulfilled")
+          .map((r) => r.value);
+        const animations = Object.assign({}, ...fulfilledResults);
+
+        setAnimations(animations);
+      } catch (error) {
+        console.error("Lỗi khi tải icon động:", error);
+      }
+    };
+
+    fetchAnimations();
+  }, []);
+
   return (
     <Animated.View entering={FadeInDown.duration(500).springify()}>
       <View className="flex flex-row justify-between items-center m-5">
-        <Text className="text-lg font-bold">Dịch vụ</Text>
+        <Text className="text-lg font-bold">Dịch vụ sửa chữa</Text>
         <Text className="text-textBlue font-semibold">Xem tất cả</Text>
       </View>
       <ScrollView
@@ -28,7 +63,7 @@ const ListService = ({ activeCategory, setActiveCategory }) => {
         className="space-x-4"
         contentContainerStyle={{ paddingHorizontal: 15 }}
       >
-        {categoryData.map((cat, index) => {
+        {activeCategory2.map((cat, index) => {
           let isActive = cat.name == activeCategory;
           let activeButtonClass = isActive ? "bg-primary" : "bg-black/10";
           return (
@@ -38,16 +73,21 @@ const ListService = ({ activeCategory, setActiveCategory }) => {
               className="flex items-center space-y-1 mr-3"
             >
               <View className={"rounded-full p-[6px] " + activeButtonClass}>
-                <LottieView
-                  source={cat.image}
-                  autoPlay
-                  loop
-                  style={{ width: hp(6), height: hp(6) }}
-                />
+                {animations[cat.name] && (
+                  <LottieView
+                    key={
+                      activeCategory === cat.name
+                        ? "active"
+                        : `inactive-${index}`
+                    }
+                    source={animations[cat.name]}
+                    autoPlay={activeCategory === cat.name}
+                    loop={activeCategory === cat.name}
+                    style={{ width: 50, height: 50 }}
+                  />
+                )}
               </View>
-              <Text className="text-neutral-600" style={{ fontSize: hp(1.6) }}>
-                {cat.name}
-              </Text>
+              <Text className="text-neutral-600">{cat.name}</Text>
             </TouchableOpacity>
           );
         })}
